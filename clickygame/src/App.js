1ce327b931c9466dc2eefb3479c123892b1c50fd
AshSeitz <ash.seitz94@gmail.com>
2019-02-10 13:10:51 -0500

    git commit -m "initial files setup, react app created"

---
+++ clickygame/src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import Image from "../components/Images"
+
+const starTrek = [
+  {
+    id: 1,
+    name: "Milk",
+    purchased: true
+  },
+  {
+    id: 2,
+    name: "Eggs",
+    purchased: true
+  },
+  {
+    id: 3,
+    name: "Cheese",
+    purchased: false
+  },
+  {
+    id: 4,
+    name: "Cake Mix",
+    purchased: false
+  },
+  {
+    id: 5,
+    name: "Carrots",
+    purchased: false
+  },
+  {
+    id: 6,
+    name: "Juice",
+    purchased: true
+  }
+];
+
+function App() {
+  return <Image starTrek={starTrek} />;
+}
+
+export default App;
